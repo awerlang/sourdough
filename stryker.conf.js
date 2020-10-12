1672a02314efc513d2ad7115ed87b8c397bfb874
André Werlang <beppe85@gmail.com>
2020-10-12 19:54:29 -0300

    chore: configure lint, test workflows

---
+++ stryker.conf.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  _comment:
+    "This config was generated using a preset. Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/vuejs.md#vuejs",
+  mutate: [
+    "src/**/*.{ts,vue}",
+    "!src/main.ts",
+    "!src/registerServiceWorker.ts"
+  ],
+  testRunner: "jest",
+  mutator: {
+    plugins: []
+  },
+  jest: {},
+  reporters: ["progress", "clear-text", "html"],
+  coverageAnalysis: "off"
+};
