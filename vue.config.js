b75729c75b809006526f09426db9f20bbde47187
André Werlang <beppe85@gmail.com>
2020-10-23 17:58:18 -0300

    chore: publish to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/sourdough/" : "/",
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].title = "Sourdough Expert";
