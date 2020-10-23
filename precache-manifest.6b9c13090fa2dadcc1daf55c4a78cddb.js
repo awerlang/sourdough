97559c3f6e54f6ac43e41d3332eee984170e47a7
awerlang <awerlang@users.noreply.github.com>
2020-10-23 21:03:24 +0000

    deploy: b75729c75b809006526f09426db9f20bbde47187

---
+++ precache-manifest.6b9c13090fa2dadcc1daf55c4a78cddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25095f4e73935389b575",
+    "url": "/sourdough/css/app.bd696bfc.css"
+  },
+  {
+    "revision": "f493fe4654fa3b8ea625cbaabcb51614",
+    "url": "/sourdough/index.html"
+  },
+  {
+    "revision": "25095f4e73935389b575",
+    "url": "/sourdough/js/app.883a35e3.js"
+  },
+  {
+    "revision": "b9785ca002f6dac119f4",
+    "url": "/sourdough/js/chunk-vendors.73942ae1.js"
+  },
+  {
+    "revision": "186bb4817398ae0c3a01c79a63ddae85",
+    "url": "/sourdough/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sourdough/robots.txt"
+  }
+]);
